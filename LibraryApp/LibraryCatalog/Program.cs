7b5fdca670e7f93283740c51e09002d06c90381a
indrajanwars <indrajanwarsetyadi@gmail.com>
2023-09-18 11:45:08 +0700

    Update class LibraryCatalog

--- LibraryApp/LibraryCatalog/Program.cs
+++ LibraryApp/LibraryCatalog/Program.cs
@@ -13,5 +13,29 @@
         {
             bookscatalog.Remove(book);
         }
+
+        public BookLib FindBook(string title)
+        {
+            foreach (var book in bookscatalog)
+            {
+                if (string.Equals(book.Title, title, StringComparison.OrdinalIgnoreCase))
+                {
+                    return book;
+                }
+            }
+            return null;
+        }
+
+        public void ListBook()
+        {
+            Console.WriteLine("Daftar Buku dalam Katalog:");
+            foreach (var book in bookscatalog)
+            {
+                Console.WriteLine($"Judul: {book.Title}");
+                Console.WriteLine($"Penulis: {book.Author}");
+                Console.WriteLine($"Tahun Terbit: {book.PublicationYear}");
+                Console.WriteLine("---------------------------");
+            }
+        }
     }
 }
