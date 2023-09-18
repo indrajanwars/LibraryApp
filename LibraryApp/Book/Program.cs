40dd5037630bf254bdff26f3a25ec4c8c2c8c711
indrajanwars <indrajanwarsetyadi@gmail.com>
2023-09-18 11:07:21 +0700

    Tambah class Book

---
+++ LibraryApp/Book/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LibraryApp
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+
+        public Book(string title, string author, int publicationYear)
+        {
+            Title = title;
+            Author = author;
+            PublicationYear = publicationYear;
+        }
+    }
+}
